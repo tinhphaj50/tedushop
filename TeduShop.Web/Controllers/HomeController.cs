0f9b88306084ed8d9499acb7e21740a9976dfbfd
abc <d>
2023-09-20 09:47:07 +0700

    first change

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace TeduShop.Web.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+           
             return View();
         }
 
